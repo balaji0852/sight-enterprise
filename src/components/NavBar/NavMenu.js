a7bc74f0b90fbdfece0c2e12b57e8b40b7c79c45
balaji <balajikumar189@gmail.com>
2022-06-23 00:58:25 +0530

    chng\

--- src/components/NavBar/NavMenu.js
+++ src/components/NavBar/NavMenu.js
@@ -15,13 +15,13 @@ class NavMenu extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            valSideBar: true,
+            valSideBar: false,
         };
         window.addEventListener('resize', () => {
             console.log('chng');
 
             this.setState({
-                valSideBar: window.screen.width > 810 && this.state.valSideBar? true: false,
+                valSideBar: window.screen.width > 810 ? !this.state.valSideBar: false,
             });
         });
         this.manageSideBarContent = this.manageSideBarContent.bind(this);
